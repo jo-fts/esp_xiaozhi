4aff75c52fc7406df48de36588e060ea9c42c8ca
jo-fts <16642236609@163.com>
2026-02-04 17:43:47 +0800

    camera

--- main/boards/bread-compact-wifi/mcp_uart_tools.cc
+++ main/boards/bread-compact-wifi/mcp_uart_tools.cc
@@ -78,6 +78,21 @@ static void SendUartCommand(const char* cmd) {
 
 }
 
+// 全局变量用于存储接收到的字符串
+static char received_data[UART_BUF_SIZE] = {0};
+
+// 串口接收函数
+static void ReceiveUartData() {
+    uint8_t data[UART_BUF_SIZE];
+    int len = uart_read_bytes(UART_PORT_NUM, data, UART_BUF_SIZE,  pdMS_TO_TICKS(2000));
+    if (len > 0) {
+        // 将接收到的数据存储到全局变量中
+        strncpy(received_data, (char*)data, len);
+        received_data[len] = '\0';  // 确保字符串以'\0'结尾
+        ESP_LOGI(TAG, "接收到串口数据: %s", received_data);
+    }
+}
+
 // 定时器回调：自动发送停止指令
 static void AutoStopCallback(TimerHandle_t xTimer) {
     // SendUartCommand(STOP_CMD);
@@ -178,5 +193,17 @@ void RegisterMcpUartTools() {
     mcp_server.AddTool("self.uart.stop", "停止停下，发送ZK", PropertyList(), [](const PropertyList&) -> ReturnValue {
         SendStopCommand();
         return true;
+    });
+    
+    mcp_server.AddTool("self.uart.receive_data", "接收串口数据并存储", PropertyList(), [](const PropertyList&) -> ReturnValue {
+        // 触发接收串口数据
+        ReceiveUartData();
+        return true;
+    });
+
+    // 新增工具：获取接收到的数据
+    mcp_server.AddTool("self.uart.get_received_data", "获取接收到的串口数据", PropertyList(), [](const PropertyList&) -> ReturnValue {
+        // 返回接收到的数据
+        return std::string(received_data);
     });
 }
